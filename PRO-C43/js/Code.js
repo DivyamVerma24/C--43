8e6fade3dd14ca7dfe129d5f508352643a3b5efc
DivyamVerma24 <81895448+DivyamVerma24@users.noreply.github.com>
2021-05-21 18:44:54 +0530

    Add files via upload

---
+++ PRO-C43/js/Code.js
@@ -0,0 +1,20 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("O U C T N I N F", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a perticular task !!", 700,170)
+    
+
+}
